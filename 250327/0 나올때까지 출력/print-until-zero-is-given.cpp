ce10d08477fd0fff43b4cbbef8e30e9287b914d6
suyeonlee333333 <suyeon2005816@naver.com>
2025-03-27 19:01:09 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250327/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    while (true) {
+        cin >> N;
+        if (N==0) {
+            break;
+        }
+    cout << N << endl;
+    }
+
+
+    return 0;
+}
