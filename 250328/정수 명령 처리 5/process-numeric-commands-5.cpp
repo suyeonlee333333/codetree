b52130c1ee24bc9bc70beb9d07999f6b8a267a80
suyeonlee333333 <suyeon2005816@naver.com>
2025-03-28 15:10:41 +0900

    Title: 정수 명령 처리 5, Time: 11ms, Memory: 1MB, Status: Passed - Codetree

--- 250328/정수 명령 처리 5/process-numeric-commands-5.cpp
+++ 250328/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int N;
@@ -7,25 +8,27 @@ string command[10000];
 int num[10000];
 
 int main() {
-    vector<int> v1;
     cin >> N;
-
+    vector<int> v;
     for (int i = 0; i < N; i++) {
         cin >> command[i];
         if (command[i] == "push_back" || command[i] == "get") {
             cin >> num[i];
         }
+        if(command[i] == "push_back") {
+            v.push_back(num[i]);
+        }   
+        if(command[i] == "get") {
+            cout << v[num[i]-1] << endl;
+        }
+        if(command[i] == "size") {
+            cout << v.size() << endl;
+        }
+        if(command[i] == "pop_back") {
+            v.pop_back();
+        }
     }
 
-    v1.push_back(10);
-    v1.push_back(20);
-    cout << v1[0] << endl;
-    cout << v1[1] << endl;
-    cout << v1.size() << endl;
-    v1.pop_back();
-    cout << v1.size() << endl;
-    cout << v1[0] << endl;
-    cout << v1.size();
 
     return 0;
 }
