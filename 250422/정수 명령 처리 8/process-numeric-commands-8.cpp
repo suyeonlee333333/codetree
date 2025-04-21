4503592a8c0d235174f3e8f1fda0413f0c1dfae3
suyeonlee333333 <suyeon2005816@naver.com>
2025-04-22 02:08:01 +0900

    Title: 정수 명령 처리 8, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250422/정수 명령 처리 8/process-numeric-commands-8.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+int N;
+string command[10000];
+int A[10000];
+
+int main() {
+    cin >> N;
+    list <int> l;
+
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push_front" || command[i] == "push_back") {
+            cin >> A[i];
+        }
+        if (command[i]=="push_front") {
+            l.push_front(A[i]);
+        }
+        if (command[i]=="push_back") {
+            l.push_back(A[i]);
+        }
+        if (command[i]=="pop_back") {
+            cout << l.back() << endl;
+            l.pop_back();
+        }
+        if (command[i]=="pop_front") {
+            cout << l.front() << endl;
+            l.pop_front();
+        }
+        if (command[i]=="size") {
+            cout << l.size() << endl;
+        }
+        if (command[i]=="front") {
+            cout << l.front() << endl;
+        }
+        if (command[i]=="back") {
+            cout << l.back() << endl;
+        }
+        if (command[i]=="empty") {
+            cout << l.empty() << endl;
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
